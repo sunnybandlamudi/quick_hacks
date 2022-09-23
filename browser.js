d0e82111220c4fa3c670b0ece1ce8d7943adcac7
sunnybandlamudi <56831492+sunnybandlamudi@users.noreply.github.com>
2022-09-23 19:08:44 +0100

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,6 +1,6 @@
 javascript:(()=>{ 
     var script = document.createElement('script'); 
-    script.src="https://cdn.jsdelivr.net/gh/sunnybandlamudi/quick_hacks@master/increase_speed.js";
+    script.src="https://cdn.jsdelivr.net/gh/sunnybandlamudi/quick_hacks@latest/increase_speed.js";
     script.type="text/javascript"; 
     document.body.appendChild(script); 
     script.onload = function (){ init();} }
